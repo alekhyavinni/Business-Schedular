e2bf06867a79d32c02422631acfa561561094e9a
bootcamp vinni <alekhyavinni12@gmail.com>
2023-07-05 17:35:27 -0400

    comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(function schedule() {
-  var today=new Date();
+var today=new Date();
 var timetags=document.querySelectorAll('.time')
- 
-  
+var saveBtn=document.querySelectorAll('.saveBtn')
+
+//today's Date using dayjs()
+$('#currentDay').text(dayjs().format('MMM D,YYYY'));
+
+//  9 am bussiness hour
 if(timetags[0].textContent<today.getHours()){
   $('#hour-9').addClass('past')
   }
@@ -17,74 +18,83 @@ if(timetags[0].textContent<today.getHours()){
     $('#hour-9').addClass('present')
   }
 
-  if(timetags[1].textContent<today.getHours()){
-      $('#hour-10').addClass('past')
-      }
-      else if(timetags[1].textContent>today.getHours()){
-        $('#hour-10').addClass('future')
-      }
-      else if(timetags[1].textContent==today.getHours()){
-        $('#hour-10').addClass('present')
-      }
+// 10 am bussiness hour
+if(timetags[1].textContent<today.getHours()){
+  $('#hour-10').addClass('past')
+  }
+else if(timetags[1].textContent>today.getHours()){
+  $('#hour-10').addClass('future')
+  }
+else if(timetags[1].textContent==today.getHours()){
+  $('#hour-10').addClass('present')
+  }
 
-  if(timetags[2].textContent<today.getHours()){
-        $('#hour-11').addClass('past')
-        }
-        else if(timetags[2].textContent>today.getHours()){
-          $('#hour-11').addClass('future')
-        }
-        else if(timetags[2].textContent==today.getHours()){
-          $('#hour-11').addClass('present')
-        }
+//  11 am bussiness hour
+if(timetags[2].textContent<today.getHours()){
+  $('#hour-11').addClass('past')
+  }
+else if(timetags[2].textContent>today.getHours()){
+  $('#hour-11').addClass('future')
+  }
+else if(timetags[2].textContent==today.getHours()){
+  $('#hour-11').addClass('present')
+  }
 
+//  12 pm bussiness hour
   if(timetags[3].textContent<today.getHours()){
-          $('#hour-12').addClass('past')
-          }
-          else if(timetags[3].textContent>today.getHours()){
-            $('#hour-12').addClass('future')
-          }
-          else if(timetags[3].textContent==today.getHours()){
-            $('#hour-12').addClass('present')
-          }
+    $('#hour-12').addClass('past')
+    }
+  else if(timetags[3].textContent>today.getHours()){
+    $('#hour-12').addClass('future')
+    }
+  else if(timetags[3].textContent==today.getHours()){
+    $('#hour-12').addClass('present')
+    }
 
+ //  1 pm bussiness hour
   if(timetags[4].textContent<today.getHours()){
-            $('#hour-1').addClass('past')
-            }
-            else if(timetags[4].textContent>today.getHours()){
-              $('#hour-1').addClass('future')
-            }
-            else if(timetags[4].textContent==today.getHours()){
-              $('#hour-1').addClass('present')
-            }
+    $('#hour-1').addClass('past')
+    }
+  else if(timetags[4].textContent>today.getHours()){
+    $('#hour-1').addClass('future')
+    }
+  else if(timetags[4].textContent==today.getHours()){
+    $('#hour-1').addClass('present')
+    }
+
+ //  2 pm bussiness hour
   if(timetags[5].textContent<today.getHours()){
-              $('#hour-2').addClass('past')
-              }
-              else if(timetags[5].textContent>today.getHours()){
-                $('#hour-2').addClass('future')
-              }
-              else if(timetags[5].textContent==today.getHours()){
-                $('#hour-2').addClass('present')
-              }
+    $('#hour-2').addClass('past')
+    }
+  else if(timetags[5].textContent>today.getHours()){
+    $('#hour-2').addClass('future')
+    }
+  else if(timetags[5].textContent==today.getHours()){
+    $('#hour-2').addClass('present')
+    }
   
+ //  3 pm bussiness hour
   if(timetags[6].textContent<today.getHours()){
     $('#hour-3').addClass('past')
-                }
-                else if(timetags[6].textContent>today.getHours()){
-                  $('#hour-3').addClass('future')
-                }
-                else if(timetags[6].textContent==today.getHours()){
-                  $('#hour-3').addClass('present')
-                }
+    }
+  else if(timetags[6].textContent>today.getHours()){
+    $('#hour-3').addClass('future')
+    }
+  else if(timetags[6].textContent==today.getHours()){
+    $('#hour-3').addClass('present')
+    }
 
-                if(timetags[7].textContent<today.getHours()){
-                  $('#hour-4').addClass('past')
-                  }
-                  else if(timetags[7].textContent>today.getHours()){
-                    $('#hour-4').addClass('future')
-                  }
-                  else if(timetags[7].textContent==today.getHours()){
-                    $('#hour-4').addClass('present')
-                  }
+//  4 pm bussiness hour
+  if(timetags[7].textContent<today.getHours()){
+    $('#hour-4').addClass('past')
+    }
+  else if(timetags[7].textContent>today.getHours()){
+    $('#hour-4').addClass('future')
+    }
+  else if(timetags[7].textContent==today.getHours()){
+    $('#hour-4').addClass('present')
+    }
+
   //  5 pm bussiness hour
   if(timetags[8].textContent<today.getHours()){
     $('#hour-5').addClass('past')
@@ -92,15 +102,18 @@ if(timetags[0].textContent<today.getHours()){
   else if(timetags[8].textContent>today.getHours()){
     $('#hour-5').addClass('future')
     }
-  else if(timetags[8].textContent===today.getHours()){
+  else if(timetags[8].textContent==today.getHours()){
     $('#hour-5').addClass('present')
     }
-  
-  
-  
-  
+ 
+for(let i=0;i<8;i++){
+  saveBtn[i].addEventListener('click',function(){
+    
+  })
+}    
+
 console.log(timetags[0].textContent)
- $('#currentDay').text(dayjs().format('MMM D,YYYY'));
+
 }
 );
 
